df4b932128b336faf6fb3fde10b08cfd58d70efe
Douglas Zinger <djzinger711@gmail.com>
2020-04-20 17:07:11 -0500

    Moved play function to setup

--- zoeySketch.js
+++ zoeySketch.js
@@ -6,7 +6,7 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight,WEBGL);
-    background(220);
+    scarySound.play();
   }
   
   function windowResized() {
@@ -17,7 +17,6 @@ function setup() {
     background(200);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
-    scarySound.play();
     sphere(150);
   }
   
